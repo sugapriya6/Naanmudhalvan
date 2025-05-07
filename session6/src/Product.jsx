8e09c987cd4a73e046c2ccc1e1af8da27d78b5e7
sugapriya6 <161485384+sugapriya6@users.noreply.github.com>
2025-05-07 19:16:55 +0530

    Update Product.jsx

--- session6/src/Product.jsx
+++ session6/src/Product.jsx
@@ -1,51 +1,64 @@
 import React from 'react'
-import AddToCartBtm from './AddToCartBtm'
 
 export default function Product() {
-    const products = [
-        {
-            id:1,
-            brand:"samsung",
-            model:"s24 ultra",
-            price:100000,
-            image:"https://th.bing.com/th/id/OIP.i3dZPTG8ooYywpOJCSzE3gHaHZ?rs=1&pid=ImgDetMain",
-            descreption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ut!"
-        },
-        {
-            id:2,
-            brand:"Apple",
-            model:"iphone 16",
-            price:60000,
-            image:"https://th.bing.com/th/id/OIP.K--7GuaoEhLA_TNdO_s22AHaEK?rs=1&pid=ImgDetMain",
-            descreption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ut!"
-        },
-        {
-            id:3,
-            brand:"Google",
-            model:"Pixel",
-            price:80000,
-            image:"https://th.bing.com/th/id/OIP.wgIGOMDJvt0pyOTZ1VEM5wHaEK?rs=1&pid=ImgDetMain",
-            descreption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ut!"
-        },
-    ]
   return (
-    <div className='flex justify-around'>
-        {products.map((pro)=>
-            <div className="card bg-base-100 w-96 shadow-sm">
-                <figure>
-                <img
-                    src={pro.image}
-                    alt="Shoes" />
-                </figure>
-                <div className="card-body bg-green-600">
-                <h2 className="card-title">{pro.brand}</h2>
-                <p>{pro.descreption}</p>
-                <div className="card-actions justify-end">
-                    <AddToCartBtm />
-                </div>
-                </div>
-          </div>  
-        )}
+    <div className='flex justify-around flex-wrap'>
+        <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+            <img
+            src="https://th.bing.com/th/id/OIP.jPe9vM9YTMXa-j6l1geNlwHaGk?w=214&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+            alt="mobiles" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">Samsung</h2>
+            <p> Offers a wide range of innovative smartphones, from budget to premium Galaxy devices.</p>
+            <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+            <img
+            src="https://th.bing.com/th/id/OIP.vaIQKuCKuLStEiFxEXehZwHaHa?w=223&h=220&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+            alt="mobiles" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">Infinix</h2>
+            <p>Budget-friendly brand known for stylish phones with strong battery life.</p>
+            <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+            <img
+            src="https://th.bing.com/th/id/OIP.JYriuVLeEpcTNpGjWFTFBwHaHa?w=189&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+            alt="mobiles" />
+        </figure>
+        <div className="card-body ">
+            <h2 className="card-title">Apple</h2>
+            <p>Premium smartphones with powerful performance and a seamless iOS experience.</p>
+            <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+            <img
+            src="https://adminapi.applegadgetsbd.com/storage/media/large/Vivo-x100-Asteroid-Black-7065.jpg"
+            alt="mobiles" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">Vivo</h2>
+            <p>Focuses on camera-centric phones with sleek design and modern features.</p>
+            <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        </div>
     </div>
   )
 }
